2be38ca04962778f7f9b37b88c88228c01b412f7
Malgorzata Marzynska <ma.marzynska@gmail.com>
2023-04-28 19:37:32 +0200

    create router and root page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const router = createBrowserRouter([]);
 
-  return (
-    <>
-  
-    </>
-  )
+  return <RouterProvider router={router} /> 
 }
 
 export default App
